22923f13911a455c3979f6f2573f93ce6f4e3cb6
Attir29 <attirmidziislamipratama@gmail.com>
2025-11-18 14:59:20 +0700

    navbar lagi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Navbar from "./components/navBar.jsx";
+import Navbar from "./components/Navbar.jsx";
 import { TypeFilter } from "./components/TypeFilter";
 
 function App() {
