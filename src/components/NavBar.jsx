f47f49f823ff12e2c28249912e89a80cdccc971e
Attir29 <attirmidziislamipratama@gmail.com>
2025-11-18 14:44:22 +0700

    nambahin navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ export default function Navbar({
 }) {
   return (
     <nav className="w-full bg-gray-900 shadow-md py-4 top-0 fixed z-50">
-      <div className="max-w-[768px] px-4 mx-auto text-center">
+      <div className="max-w-3xl px-4 mx-auto text-center">
         <h1 className="text-2xl font-bold mb-3">Pokédex</h1>
 
         <div className="flex gap-2">
